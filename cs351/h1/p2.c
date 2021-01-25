4729015e3542b996ac02a3203da02da3ea145e54
scheisseosu <jjwazoo@gmail.com>
2021-01-25 14:41:11 -0500

    p2

--- cs351/h1/p2.c
+++ cs351/h1/p2.c
@@ -17,7 +17,18 @@
 
 int main(void)
 {
-
+    for(int i=0; i<32; i++){
+        uint32_t n = 1;
+        n = n<<i;
+        printf("%2d: ",i);
+        //print binary representation
+        uint32_t mask = 1<<31;  //mask starting at 32nd bit to fill leading zeros
+        while(mask){
+            printf("%d",(mask & n) != 0);
+            mask = mask>>1;
+        }
+        printf(" %d\n", n);
+    }
 
   return 0;
 }
