73a702f7f52a32aeb5aee5ce3a3a84db6831ca7f
scheisseosu <jjwazoo@gmail.com>
2021-03-15 21:29:06 -0400

    hi

--- cs351/h5/h5.s
+++ cs351/h5/h5.s
@@ -2,8 +2,42 @@
 
 extern puts, putchar, exit, printnum
 
+SECTION .bss
+    statbuf: resb 144
+
+SECTION .data
+    usage: db`Usage: h5 <file>\n`,0
+    unable: db`Unable to stat file.\n`,0
+    newl: db`\n`,0
+
 SECTION .text
 
 global _start
 _start:
+    mov r15, [rsp]
+    cmp r15, 2
+    jge .hasargs
+    mov rsi, usage
+    call puts
+    mov rdi, 1
+    call exit
+    .hasargs:
+    mov rax, 4
+    mov rdi, [rsp+16]
+    mov rsi, statbuf
+    syscall
+    cmp rax, 0
+    jge .printfields
+    mov rsi, unable
+    call puts
+    mov rdi, 1
+    call exit
 
+    .printfields:
+    mov al, byte[statbuf+8]
+    call printnum
+    mov rsi, newl
+    call puts
+    .exit:
+        mov rdi,0
+        call exit
